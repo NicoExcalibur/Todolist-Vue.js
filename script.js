15ae976168df4e1cdc00bf743abe593f8e75d5e5
NicoExcalibur <nico.zanz@gmail.com>
2020-12-03 18:57:13 +0100

    function adding new note

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
 const vm = Vue.createApp({
     data(){
         return {
-            message: 'hello t\'a vu'
+            todos: [
+                'Sauver le monde',
+                'Apprendre VueJS',
+                'Faire à manger à Lou'
+            ]
+        }
+    },
+    methods: {
+        invert(){
+            this.todos.reverse();
+        },
+        addNote(message){
+            this.todos.push(message);
         }
     }
-}).mount('#app');
+})
+
+vm.component('note', {
+    props: ['content'],
+    template: `<p> {{ content }} </p>`
+});
+
+vm.component('add', {
+    props: [],
+    emits: ['newNote'],
+    data(){
+        return{
+            intern: 'Nouveau Message'
+        }
+    },
+    methods:{
+        saveNote(){
+            this.$emit('newnote', this.intern)
+            this.intern = '';
+        }
+    },
+    template: `
+        <input type="text" v-model="intern"/>
+        <a href="#" @click="saveNote" v-if="intern != '' ">Ajouter</a>
+        {{ intern }}
+    `
+});
+
+vm.mount('#app');
